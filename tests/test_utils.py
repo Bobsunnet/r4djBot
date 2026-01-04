a5490f0eddd3900254be95c5c5de3e501bfa33df
bob <bobsunnet@gmail.com>
2026-01-04 15:35:45 +0200

    test: add test to order text message

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,4 @@
-from utils.utils import format_welcome_message
+from utils.utils import format_order_message, format_welcome_message
 
 
 def test_format_welcome_message():
@@ -8,4 +8,24 @@ def test_format_welcome_message():
     name = "Олександр"
     expected = "Вітаємо, Олександр. Оберіть одну з команд:"
     result = format_welcome_message(name)
+    assert result == expected
+
+
+def test_format_order_message():
+    """
+    Test that the order message is formatted correctly with items.
+    """
+    user_full_name = "Бобер Маслов"
+    username = "bobermaslov"
+    phone_number = "+380999999999"
+    items = [{"name": "Speaker", "quantity": 2}, {"name": "Mixer", "quantity": 1}]
+
+    expected = (
+        "Заказ від Бобер Маслов @bobermaslov\n"
+        "+380999999999\n\n"
+        "• Speaker × 2 шт.\n"
+        "• Mixer × 1 шт.\n"
+    )
+
+    result = format_order_message(user_full_name, username, phone_number, items)
     assert result == expected
