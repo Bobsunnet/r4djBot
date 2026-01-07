f1771c21dfee0016e12f9d3855aedf9a35474523
bob <bobsunnet@gmail.com>
2026-01-07 06:57:47 +0200

    fix: change interval db sync tp 10 min

--- schedulers/schedulers.py
+++ schedulers/schedulers.py
@@ -5,5 +5,5 @@ from db_handler.db_class import db_handler
 
 def scheduler_setup():
     scheduler = AsyncIOScheduler()
-    scheduler.add_job(db_handler.update_items_table, "interval", minutes=1)
+    scheduler.add_job(db_handler.update_items_table, "interval", minutes=10)
     scheduler.start()
