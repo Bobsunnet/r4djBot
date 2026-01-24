d1de2c47f30fc157a88ed4d04231e40eeba0c1f4
bob <bobsunnet@gmail.com>
2026-01-24 16:59:50 +0200

    test: change tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,7 @@
 from datetime import date
 
+from db_handler.schemas.order import Order
+from db_handler.schemas.user import User
 from utils import utils
 
 
@@ -12,6 +14,7 @@ def test_format_welcome_message():
         "Вітаємо, Олександр. Оберіть одну з команд /catalogue, /contacts, /order:"
     )
     result = utils.format_welcome_message(name)
+
     assert result == expected
 
 
@@ -19,22 +22,30 @@ def test_format_order_message():
     """
     Test that the order message is formatted correctly with items.
     """
-    name = "Бобер"
-    surname = "Маслов"
-    username = "bobermaslov"
-    phone_number = "+380999999999"
-    start_date = "01.01.24"
-    end_date = "01.01.24"
-    count = 1
-    address = "Київ"
+    user = User(
+        id=1,
+        name="Бобер",
+        surname="Маслов",
+        username="bobermaslov",
+        phone_number="+380999999999",
+        user_id=123456789,
+    )
+    order = Order(
+        id=1,
+        user_id=1,
+        date_start=utils.validate_date("01.01.24"),
+        date_end=utils.validate_date("02.01.24"),
+        work_days=1,
+        address="Київ",
+        description="comment text",
+    )
     items = [{"name": "Speaker", "quantity": 2}, {"name": "Mixer", "quantity": 1}]
-    comment = "comment text"
 
     expected = (
         "Замовлення від Бобер Маслов @bobermaslov\n"
         "+380999999999\n\n"
-        "Початок оренди: 01.01.24\n"
-        "Кінець оренди: 01.01.24\n"
+        "Початок оренди: 2024-01-01\n"
+        "Кінець оренди: 2024-01-02\n"
         "Кількість днів роботи: 1\n"
         "Адреса та час доставки/самовивіз: Київ\n\n"
         "Коментар: comment text\n\n"
@@ -42,18 +53,8 @@ def test_format_order_message():
         "• Mixer × 1 шт.\n"
     )
 
-    result = utils.format_order_message(
-        name,
-        surname,
-        username,
-        phone_number,
-        start_date,
-        end_date,
-        count,
-        address,
-        comment,
-        items,
-    )
+    result = utils.format_order_message_for_admin(user, order, items)
+
     assert result == expected
 
 
@@ -61,9 +62,11 @@ def test_extract_date():
     """
     Test that the extract_date function correctly extracts the start and end dates from a date string.
     """
+
     date_str = "01.01.24 - 01.01.24"
     expected = ("01.01.24", "01.01.24")
     result = utils.extract_date(date_str)
+
     assert result == expected
 
 
@@ -74,4 +77,5 @@ def test_validate_date():
     date_str = "01.01.24"
     expected = date(2024, 1, 1)
     result = utils.validate_date(date_str)
+
     assert result == expected
